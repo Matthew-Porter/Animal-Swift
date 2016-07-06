ec2833f99cd727e56e74f3d63db093c614836610
Matthew Porter <matthewpor@gmail.com>
2016-07-06 08:16:13 -0700

    Initial Commit

---
+++ Animal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Animal
+//
+//  Created by matthew porter on 7/6/16.
+//  Copyright © 2016 matthew porter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
